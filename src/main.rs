9b1203888185de9245dc7f6d7664032c5677df5a
Tim Day <tday141@gmail.com>
2023-02-02 01:08:18 -0500

    Add function to find and parse default.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use serde::{Deserialize, Serialize};
 use std::process::Command;
+use std::path::PathBuf;
+use serde_yaml::{self};
 use clap::Parser;
 use std::fs;
 
@@ -15,13 +18,37 @@ struct Args {
     count: u8,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    add: Vec<String>,
+    remove: Vec<String>
+}
+
 fn main() -> Result<(), ()> {
+    let mut config_directory = PathBuf::new();
     let args = Args::parse();
     let mut failed = false;
 
-    match create_config_directory(args.verbose) {
-	Ok(_) => if args.verbose {
-	    println!("Found or created configuration directory.");
+    match check_config_directory(args.verbose) {
+	Ok(i) => {
+	    config_directory = i;
+	    if args.verbose {
+		println!("Found or created configuration directory.");
+	    }
+	},
+	Err(e) => {
+	    println!("{e}");
+	    failed = true;
+	},
+    };
+
+    if failed {
+	return Err(());
+    }
+
+    match check_global_config(args.verbose, config_directory) {
+	Ok(_i) => if args.verbose {
+	    println!("Found or created default configuration.");
 	},
 	Err(e) => {
 	    println!("{e}");
@@ -54,7 +81,7 @@ fn main() -> Result<(), ()> {
     Ok(())
 }
 
-fn create_config_directory(verbose:bool) -> Result<(), String> {
+fn check_config_directory(verbose:bool) -> Result<PathBuf, String> {
     match home::home_dir() {
 	Some(mut path) => {
 	    path.push(".chack");
@@ -63,11 +90,24 @@ fn create_config_directory(verbose:bool) -> Result<(), String> {
 		println!("{}", path.display());
 	    }
 
-	    match fs::create_dir_all(path) {
-		Ok(_) =>  Ok(()),
+	    match fs::create_dir_all(path.clone()) {
+		Ok(_) =>  Ok(path),
 		Err(_) => Err("Could not create config directory!".to_string())
 	    }
 	},
 	None => Err("Impossible to get your home dir!".to_string()),
     }
+}
+
+fn check_global_config(verbose:bool, mut path:PathBuf) -> Result<(), String> {
+    path.push("default.yaml");
+
+    let f = std::fs::File::open(path).expect("Could not open file.");
+    let scrape_config:Config = serde_yaml::from_reader(f).expect("Could not read values.");
+
+    if verbose {
+	println!("{:?}", scrape_config);
+    }
+
+    Ok(())
 }
