5d7ba8a1b1819491449dc4da35151a4129339bd5
Tim Day <tday141@gmail.com>
2023-02-01 23:43:32 -0500

    Initial wiring for arg parsing and running commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::process::Command;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let mut echo_hello = Command::new("echo");
+
+    echo_hello.arg("-n");
+    echo_hello.arg("Hello");
+    echo_hello.arg(format!("{}!", args.name));
+
+    let hello_1 = echo_hello.output().expect("failed to execute process");
+    let output = match std::str::from_utf8(&hello_1.stdout) {
+	Ok(i) => i,
+	Err(_e) => "Error!"
+    };
+
+    for _ in 0..args.count {
+	println!("{output}");
+    }
 }
